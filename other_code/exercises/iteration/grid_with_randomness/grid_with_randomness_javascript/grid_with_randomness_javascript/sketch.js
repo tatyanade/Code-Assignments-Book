8f89399bebc792ed7b1832ac9c26fc0f7395e9c8
Tatyana <tmustako@gmail.com>
2017-09-16 19:51:21 -0700

    Add files via upload
    grid with randomness py & js

---
+++ other_code/exercises/iteration/grid_with_randomness/grid_with_randomness_javascript/grid_with_randomness_javascript/sketch.js
@@ -0,0 +1,28 @@
+// Grid with Randomness
+
+function setup() {
+  createCanvas(400, 400);
+  smooth();
+  noLoop(); 
+}
+
+function draw() {
+  background(255); 
+  strokeWeight(2);
+  stroke(0); 
+  noFill(); 
+  
+  for (var y=0; y<8; y++) {
+    for (var x=0; x<8; x++) {
+      var px = x * 50; 
+      var py = y * 50; 
+      
+      var chance = random(1); 
+      if (chance < 0.05){
+        ellipse(px+25, py+25, 25, 25);
+      } else {
+        rect(px+5, py+5, 40, 40);
+      }
+    }
+  }
+}

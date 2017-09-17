6dac14548933de9c014093c3eda351cf2f93a4db
Tatyana <tmustako@gmail.com>
2017-09-16 19:42:52 -0700

    Add files via upload
    js & py cursor lines

---
+++ other_code/exercises/iteration/lines_to_the_cursor/lines_to_the_cursor_javascript/lines_to_the_cursor_javascript/sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(400, 400); 
+  smooth();
+}
+
+function draw() {
+  background (255); 
+
+  strokeWeight (2.0);
+  stroke(0); 
+
+  var nLines = 10;
+  var margin = 50; 
+  for (var i=1; i<=nLines; i++) {
+    var x0 = map(i, 1,nLines, margin, width-margin); 
+    var y0 = margin; 
+    line(x0, y0, mouseX, mouseY);
+  }
+}
